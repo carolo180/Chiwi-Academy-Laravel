529ef238f9b9f456f68cb27106589f88e94e820b
Trueqo <julianochoa1999@gmail.com>
2022-04-19 15:50:36 -0500

    Footer finished

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,5 +1,5 @@
-<div class="h-14 w-full bg-red-900 text-center items-center bottom-0 absolute">
-    <h1 class="text-white text-2xl font-AbhayaLibre">
+<div class="h-14 w-full bg-black flex justify-center items-center bottom-0 absolute">
+    <h1 class="text-white text-xl font-AbhayaLibre">
         © Chiwi Acádemy - All rights reserve
     </h1>
 </div>
