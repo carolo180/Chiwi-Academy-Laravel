eb17c6656300d2a711134ecc9189d3ce64d2cd96
Trueqo <julianochoa1999@gmail.com>
2022-04-19 12:32:34 -0500

    component Footer added

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,5 @@
+<div class="w-full h-20 flex items-center">
+    <h1 class="text-3xl font-bold text-white">
+        Chiwi Academy
+    </h1>
+</div>
--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,5 +1,5 @@
 <div class="bg-[#000] h-20 flex items-center justify-center" >
-    <h1 class="text-3xl font-bold text-white">
-        Chiwi Academy
+    <h1 class="text-3xl font-bold text-white bottom-0">
+        © Chiwi Acádemy - All rights reserve
     </h1>
 </div>
