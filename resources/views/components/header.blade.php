04df264c1483aa7435f2c7f9e8bf1f33173b4e5d
carolo180 <caponeris1@gmail.com>
2022-04-20 09:29:30 -0500

    upload landingpage Admin with Admin LTE

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,4 +1,4 @@
-<div class="bg-[#000] h-20 flex items-center justify-center" >
+<div class="bg-[#000] h-24 flex items-center justify-center" >
     <h1 class="text-3xl font-bold text-white">
         Chiwi Academy
     </h1>
